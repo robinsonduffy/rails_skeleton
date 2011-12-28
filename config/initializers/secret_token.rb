169e89b8cb0fae46b189f14221b59f99b0f166b2
Robinson Duffy <robinson.duffy@gmail.com>
2011-12-27 19:45:42 -0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stampson::Application.config.secret_token = '30009c4e954383a9255f4a189748f337491ec5a8720f964cd9fbc2a564ac3b3bf8467e7d716444fc157c4127d97c104ca291b53daf1b379c41d82ec545ada14e'
