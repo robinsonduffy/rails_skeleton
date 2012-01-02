32d1bc6fcb3e34f41b6f27a013d6de6b8c14b70c
Robinson Duffy <robinson.duffy@gmail.com>
2012-01-02 14:37:24 -0900

    Login and Logout buttons

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,6 +12,27 @@ describe PagesController do
       get :home
       response.should have_selector("title", :content => @base_title)
     end
+    
+    describe "for non-logged in users" do
+      it "should have a login button" do
+        get :home
+        response.should have_selector("a", :content => "Login", :href => login_path)
+        response.should_not have_selector("a", :content => "Logout", :href => logout_path)
+      end
+    end
+    
+    describe "for logged in users" do
+      before(:each) do
+        login_user(Factory(:user))
+      end
+      
+      it "should have a logout button" do
+        get :home
+        response.should_not have_selector("a", :content => "Login", :href => login_path)
+        response.should have_selector("a", :content => "Logout", :href => logout_path)
+      end
+    end
+        
   end
 
 end
